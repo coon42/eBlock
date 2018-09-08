0807d58f338d13d849c49b5be4889c266ab0bb11
coon <coon@c-base.org>
2018-09-08 14:37:46 +0200

    chrome: add warnings of blacklisted seller on ebay page

--- chromium/background.js
+++ chromium/background.js
@@ -11,7 +11,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'}, // TODO: add ebay here
+        pageUrl: {hostEquals: 'www.ebay.de'}, // TODO: also support other countries
       })
       ],
           actions: [new chrome.declarativeContent.ShowPageAction()]
