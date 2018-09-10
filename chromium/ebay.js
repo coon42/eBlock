80f1d577b4c5e3261d6b67e662674ba0f54ec365
coon <coon@c-base.org>
2018-09-10 10:55:52 +0200

    blacklist script is executed automatically

---
+++ chromium/ebay.js
@@ -0,0 +1,56 @@
+function removeAllEventListenersFromElement(element) {
+  let clone = element.cloneNode();
+
+  while (element.firstChild)
+    clone.appendChild(element.lastChild);
+
+  element.parentNode.replaceChild(clone, element);
+}
+
+function getBlacklist() {
+  let blacklist = JSON.parse('{"users": ["welovebasic_de"]}');
+
+  return blacklist;
+}
+
+function userIsBlacklisted(userName) {
+  let blacklist = getBlacklist();
+
+  console.log('blacklist: ');
+  console.log(blacklist);
+
+  for (let i in blacklist.users) {
+    console.log(blacklist.users[i]);
+    console.log(userName);
+
+    if (blacklist.users[i] == userName)
+      return true;
+  }
+
+  return false;
+}
+
+var sellerName = document.getElementById('mbgLink').getElementsByTagName('span')[0].innerHTML;
+
+console.log('detected seller: ');
+console.log(sellerName.innerHTML);
+
+function disableButton(buttonElement) {
+  buttonElement.setAttribute('disabled', '');
+  buttonElement.style = 'color:yellow';
+  buttonElement.innerHTML = 'Seller is blacklisted';
+
+  removeAllEventListenersFromElement(buttonElement);
+}
+
+if (userIsBlacklisted(sellerName)) {
+  sellerName.style = 'color:red !important';
+
+  var buyButton = document.getElementById('binBtn_btn');
+  var cartButton = document.getElementById('isCartBtn_btn');
+  var barterButton = document.getElementById('boBtn_btn');
+
+  disableButton(buyButton);
+  disableButton(cartButton);
+  disableButton(barterButton);
+}
