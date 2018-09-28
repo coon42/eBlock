127135004f2c0d5a6bb9845a2896fcf6db99e87f
coon <coon@mailbox.org>
2018-09-29 00:15:30 +0200

    render warning box for blocked users

--- chromium/ebay.js
+++ chromium/ebay.js
@@ -1,12 +1,19 @@
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
 function getBlacklist() {
   // TODO: get blacklist from local storage or database:
 
   let blacklist = {};
   let users = {};
-  blacklist.users = users;
+  let user = {};
 
-  let reason = "bad shipping";
-  users["welovebasic_de"] = reason;
+  user["date"] = "2018-09-28";
+  user["reason"] = "DPD did not leave a message after failed delivery. Seller wanted 10€ extra for another shipping attempt and refused cancellation. Seller has lots of negative feedback with similiar issues.";
+  users["welovebasic_de"] = user;
+
+  blacklist.users = users;
 
   return blacklist;
 }
@@ -25,7 +32,21 @@ function disableButton(buttonElement) {
   buttonElement.style = 'pointer-events: none;'
 }
 
-var sellerElement = document.getElementById('mbgLink');
+function renderWarningBox(sellerName, user) {
+  let warningBoxContainer = getElementByXpath('//*[@id="mainContent"]/form/div[2]/div[1]/table/tbody/tr/td[2]');
+  let warningBoxPosition = getElementByXpath('//*[@id="mainContent"]/form/div[2]/div[1]/table/tbody/tr/td[2]/div[2]');
+  let warningBox = document.createElement("DIV");
+
+  date = new Date(user['date']);
+  dateStr = date.toLocaleDateString("de-DE");
+
+  warningBox.innerHTML = '<div>Seller was blacklisted on ' + dateStr + '.</div><div style="margin-top: 15px;">Reason: ' + user['reason'] + '</div>';
+  warningBox.style = 'border: 1px solid black; background-color: yellow; padding: 10px; margin-bottom: 10px';
+
+  warningBoxContainer.insertBefore(warningBox, warningBoxPosition);
+}
+
+let sellerElement = document.getElementById('mbgLink');
 
 if(sellerElement != null) {
   let sellerName = sellerElement.getElementsByTagName('span')[0].innerHTML;
@@ -41,6 +62,9 @@ if(sellerElement != null) {
     disableButton(buyButton);
     disableButton(cartButton);
     disableButton(barterButton);
+
+    user = getBlacklist().users[sellerName];
+    renderWarningBox(sellerName, user);
   }
 }
 
